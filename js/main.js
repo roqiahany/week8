b02f2bf4c3d9ea129601d01f2448ff67ca995c4f
Roqia Hany <roqiahani5@gmail.com>
2024-02-04 00:35:32 +0200

    method of Array and Create and display data (CRUD)

--- js/main.js
+++ js/main.js
@@ -1,34 +1,54 @@
-function calcPrice(price, ads, tax) {
-  var sum = price + ads;
-  var multi = sum * tax;
-  console.log(multi);
-  return multi;
+// var friends = ['alaa', 'mai', 'roqa', 'toto'];
+// var x = friends.pop();
+//var x = friends.shift();
+//var x = friends.push();
+// var x = friends.unshift();
+// var x = friends.length;
+//var x = friends.indexOf('alaa');
+// var x = friends.lastIndexOf('alaa');
+//var x = friends.includes('alaa');
+//var x = friends.slice(1,3);
+//var x = friends.splice(2,1); // delete
+// var x = friends.splice(3,0,'koko');//add
+// var x = friends.splice(2, 1, 'koko'); // add,delete
+
+// console.log(friends);
+// console.log(x);
+
+var nameProductInput = document.getElementById('productName');
+var priceProductInput = document.getElementById('productPrice');
+var categoryProductInput = document.getElementById('productCategory');
+var descriptionProductInput = document.getElementById('productDescription');
+// console.log(nameProductInput);
+
+var producrContainer = [];
+
+function addProduct() {
+  var product = {
+    name: nameProductInput.value,
+    price: priceProductInput.value,
+    cate: categoryProductInput.value,
+    desc: descriptionProductInput.value,
+  };
+  producrContainer.push(product);
+  console.log(producrContainer);
 }
 
-// decleration
-// function getPrice(num1, num2) {
-//   var sum = num1 + num2;
-//   return sum;
-// }
-
-// expression
-// var getPrice = function () {
-//   var sum = num1 + num2;
-//   return sum;
-// };
-
-// calcPrice(getPrice(100, 200), 200, 2);
-
-function foo() {
-  function bar() {
-    return 3;
+function displayData() {
+  var cartona = '';
+  for (var i = 0; i < 4; i++) {
+    cartona += `<tr>
+              <td>oppo</td>
+              <td>9000</td>
+              <td>mob</td>
+              <td>yyyyyy</td>
+              <td>
+                <button class="btn btn-outline-warning btn-sm">update</button>
+                <button class="btn btn-outline-danger btn-sm">delete</button>
+              </td>
+            </tr>`;
   }
-
-  return bar();
-
-  function bar() {
-    return 8;
-  }
+  document.getElementById('tableData').innerHTML = cartona;
 }
 
-window.alert(foo());
+displayData();
