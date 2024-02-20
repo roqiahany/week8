0aab2eb85dbd8ed5a7dd53473d7cd66683da51aa
Roqia Hany <roqiahani5@gmail.com>
2024-02-20 07:22:45 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,85 +1,91 @@
-// to day
-var todayByName = document.getElementById('todayByName'); //done
-var todayByNum = document.getElementById('todayByNum'); //done
-var todayInMonth = document.getElementById('todayInMonth'); //done
-var todayLocation = document.getElementById('todayLocation'); //done
-var todayTemp = document.getElementById('todayTemp'); //done
-var todayConditionImg = document.getElementById('todayConditionImg'); //done
-var todayConditionText = document.getElementById('todayConditionText'); //done
-var humidity = document.getElementById('humidity'); //done
-var wind = document.getElementById('wind'); //done
-var windDirection = document.getElementById('windDirection'); //done
+var inputTask = document.getElementById('inputTask');
+var taskDate = document.getElementById('taskDate');
+var btnAdd = document.getElementById('btnAdd');
 
-// next Data
-var nextDayName = document.getElementsByClassName('nextDayName');
-var nextMaxTemp = document.getElementsByClassName('nextMaxTemp'); //done
-var nextMinTemp = document.getElementsByClassName('nextMinTemp'); //done
-var nextConditionText = document.getElementsByClassName('nextConditionText'); //done
-var nextConditionImg = document.getElementsByClassName('nextConditionImg'); //done
-
-// input
-var searchInput = document.getElementById('searchFined');
-
-// date
-// var date = new Date();//current data
-// console.log(date.getDate());
-// console.log(date.toLocaleDateString('en-us', { weekday: 'long' }));
-// console.log(date.toLocaleDateString('en-us', { month: 'long' }));
-
-// function fetch
-async function getDataWeather(cityName) {
-  var dataResponse = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=d92319e7b9f74ac984362613241802&q=${cityName}&days=3`
-  );
-  var dataWeather = await dataResponse.json();
-  return dataWeather;
+if (localStorage.getItem('tasks') != null) {
+  containerTask = JSON.parse(localStorage.getItem('tasks'));
+  displayTask();
+} else {
+  var containerTask = [];
 }
-// function display today data
-function displayTOday(data) {
-  var todayDate = new Date();
-  todayByName.innerHTML = todayDate.toLocaleDateString('en-us', {
-    weekday: 'long',
-  });
-  todayInMonth.innerHTML = todayDate.toLocaleDateString('en-us', {
-    month: 'long',
-  });
-  todayByNum.innerHTML = todayDate.getDate();
-  todayLocation.innerHTML = data.location.name;
-  todayTemp.innerHTML = data.current.temp_c;
-  todayConditionImg.setAttribute('src', data.current.condition.icon);
-  todayConditionText.innerHTML = data.current.condition.text;
-  humidity.innerHTML = data.current.humidity + '%';
-  wind.innerHTML = data.current.wind_kph + 'km/h';
-  windDirection.innerHTML = data.current.wind_dir;
-}
-// function display next days
-function displayNextDay(data) {
-  for (var i = 0; i < 2; i++) {
-    var nextDate = new Date(data.forecast.forecastday[i + 1].date);
-    nextDayName[i].innerHTML = nextDate.toLocaleDateString('en-us', {
-      weekday: 'long',
+
+function addTask() {
+  if (validateDate() && validateTask()) {
+    Swal.fire({
+      position: 'top-center',
+      icon: 'success',
+      title: 'Your work has been saved',
+      showConfirmButton: false,
+      timer: 1500,
     });
-    nextMaxTemp[i].innerHTML = data.forecast.forecastday[i + 1].day.maxtemp_c;
-    nextMinTemp[i].innerHTML = data.forecast.forecastday[i + 1].day.mintemp_c;
-    nextConditionText[i].innerHTML =
-      data.forecast.forecastday[i + 1].day.condition.text;
-    nextConditionImg[i].setAttribute(
-      'src',
-      data.forecast.forecastday[i + 1].day.condition.icon
-    );
+    var task = {
+      tittle: inputTask.value,
+      date: taskDate.value,
+    };
+    containerTask.push(task);
+    localStorage.setItem('tasks', JSON.stringify(containerTask));
+    displayTask();
+    clear();
   }
 }
 
-// function start App
-async function startApp(city = 'cairo') {
-  var data = await getDataWeather(city);
+function displayTask() {
+  var cartona = '';
+  for (var i = 0; i < containerTask.length; i++) {
+    cartona += `<div class="col-lg-12">
+    <div class="form-check d-flex align-items-center px-3">
+      <input
+        type="checkbox"
+        class="form-check-input mx-0"
+        name=""
+        id="${containerTask[i].tittle}"
+      />
+      <label class="form-check-label h3 m-3 p-0" for="${containerTask[i].tittle}">${containerTask[i].tittle}</label>
+      <div class="d-flex ms-auto align-items-center gap-2">
+        <i
+          class="fa-solid fa-trash-can text-danger-emphasis"
+          onclick="deleteTask(${i})"
+        ></i>
+        <div class="bg-white border border-warning rounded p-2">
+          <i class="fa-solid fa-hourglass-half text-warning"> </i>
+          <span>${containerTask[i].date}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+    `;
+  }
 
-  if (!data.error) {
-    displayTOday(data);
-    displayNextDay(data);
+  document.getElementById('taskContent').innerHTML = cartona;
+}
+
+function deleteTask(index) {
+  containerTask.splice(index, 1);
+  localStorage.setItem('tasks', JSON.stringify(containerTask));
+  displayTask();
+}
+
+function clear() {
+  inputTask.value = '';
+}
+
+function validateDate() {
+  var currentDate = new Date().toJSON().slice(0, 10); //2024-02-20
+  if (taskDate.value < currentDate) {
+    Swal.fire('Enter Valid Date');
+    return false;
+  } else {
+    return true;
   }
 }
-startApp();
-searchInput.addEventListener('input', function () {
-  startApp(searchInput.value);
-});
+
+function validateTask() {
+  var regex = /.+/; // ay 7aga
+  if (regex.test(inputTask.value)) {
+    return true;
+  } else {
+    Swal.fire('Enter Task');
+    return false;
+  }
+}
